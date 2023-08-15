43eca6ee31d6ee92e4755ed14c0ace157fb311b9
Jurica Zecevic <Jurica_Zecevic@epam.com>
2023-08-15 18:54:28 +0200

    fix: Removing React import

--- src/components/Courses/components/SearchBar/SearchBar.jsx
+++ src/components/Courses/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import Input from '../../../../common/Input/Input';
 import Button from '../../../../common/Button/Button';
