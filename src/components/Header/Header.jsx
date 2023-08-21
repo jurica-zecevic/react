e53fb628760b9b3162895d4e47205f8073f8535f
Jurica Zecevic <Jurica_Zecevic@epam.com>
2023-08-21 09:07:45 +0200

    feat: Header auth functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,47 @@
+import { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
 import styles from './Header.module.css';
 
 import Logo from './components/Logo/Logo';
 import Button from '../../common/Button/Button';
 
 const Header = () => {
-	const isAuthorized = true;
+	const [isAuthorized, setIsAuthorized] = useState(false);
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+		setIsAuthorized(!!token);
+	}, []);
+
+	const handleLogout = () => {
+		localStorage.removeItem('token');
+		setIsAuthorized(false);
+		navigate('/login');
+	};
+
+	const handleLogin = () => {
+		setIsAuthorized(true);
+		navigate('/courses');
+	};
+
+	const shouldShowAuthElements = !['/login', '/register'].includes(
+		location.pathname
+	);
 
 	return (
 		<header className={styles.header}>
 			<Logo />
-			{isAuthorized && <span>Jurica Zečević</span>}
-			<Button type='button' buttonText={isAuthorized ? 'Logout' : 'Login'} />
+			{isAuthorized && shouldShowAuthElements && <span>Jurica Zečević</span>}
+			{shouldShowAuthElements && (
+				<Button
+					type='button'
+					buttonText={isAuthorized ? 'Logout' : 'Login'}
+					onClick={isAuthorized ? handleLogout : handleLogin}
+				/>
+			)}
 		</header>
 	);
 };
