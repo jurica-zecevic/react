51e884a3f458e2e881c98abd5e1e76410c733545
Jurica Zecevic <Jurica_Zecevic@epam.com>
2023-08-21 09:07:26 +0200

    feat: Empty course list functionality

--- src/components/EmptyCourseList/EmptyCourseList.jsx
+++ src/components/EmptyCourseList/EmptyCourseList.jsx
@@ -1,13 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+
 import Button from '../../common/Button/Button';
 
 import styles from './EmptyCourseList.module.css';
 
-const EmptyCourseList = () => {
+const EmptyCourseList = ({ userRole }) => {
+	const navigateAddCourse = useNavigate();
+
+	const handleAddNewCourse = () => {
+		navigateAddCourse('/courses/add');
+	};
+
 	return (
 		<section className={styles.empty}>
 			<h1>Your List Is Empty</h1>
 			<p>Please use ’Add New Course’ button to add your first course</p>
-			<Button type='button' buttonText='Add new course' />
+			{userRole === 'admin' ? (
+				<Button
+					type='button'
+					buttonText='Add new course'
+					onClick={handleAddNewCourse}
+				/>
+			) : (
+				<p>
+					You don't have permissions to create a course. Please log in as ADMIN
+				</p>
+			)}
 		</section>
 	);
 };
