1ff6d7baa2f4a362060cba1841bdfd6606858eb8
Jurica Zecevic <Jurica_Zecevic@epam.com>
2023-08-21 09:08:56 +0200

    feat: Reintroducing empty courses list

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 import SearchBar from './components/SearchBar/SearchBar';
+import EmptyCourseList from '../EmptyCourseList/EmptyCourseList';
 import CourseCard from './components/CourseCard/CourseCard';
 import Button from '../../common/Button/Button';
 
@@ -10,10 +11,12 @@ import styles from './Courses.module.css';
 const Courses = ({ coursesList, authorsList }) => {
 	const [filteredCourses, setDisplayedCourses] = useState(coursesList);
 
-	const navigate = useNavigate();
+	const navigateAddCourse = useNavigate();
+
+	const userRole = 'admin';
 
 	const handleAddNewCourse = () => {
-		navigate('/courses/add');
+		navigateAddCourse('/courses/add');
 	};
 
 	const handleSearch = (searchTerm) => {
@@ -36,25 +39,29 @@ const Courses = ({ coursesList, authorsList }) => {
 
 	return (
 		<>
-			<section>
-				<div className={styles.coursesHeader}>
-					<SearchBar onSearch={handleSearch} />
-					<div className={styles.buttonContainer}>
-						<Button
-							type='button'
-							buttonText='Add new course'
-							onClick={handleAddNewCourse}
-						/>
+			{coursesList.length ? (
+				<section>
+					<div className={styles.coursesHeader}>
+						<SearchBar onSearch={handleSearch} />
+						<div className={styles.buttonContainer}>
+							<Button
+								type='button'
+								buttonText='Add new course'
+								onClick={handleAddNewCourse}
+							/>
+						</div>
 					</div>
-				</div>
-				<ul className={styles.coursesList}>
-					{filteredCourses.map((course) => (
-						<li key={course.id}>
-							<CourseCard {...course} authorsList={authorsList} />
-						</li>
-					))}
-				</ul>
-			</section>
+					<ul className={styles.coursesList}>
+						{filteredCourses.map((course) => (
+							<li key={course.id}>
+								<CourseCard {...course} authorsList={authorsList} />
+							</li>
+						))}
+					</ul>
+				</section>
+			) : (
+				<EmptyCourseList userRole={userRole} />
+			)}
 		</>
 	);
 };
