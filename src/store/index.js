8bb363ce2910ef0ad3aa0852f5314af8e6808a19
Jurica Zecevic <Jurica_Zecevic@epam.com>
2023-08-24 15:05:30 +0200

    feat: root reducer, store index setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,8 @@
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
-import { coursesInitialState } from './courses/reducer.js';
 
-const appInitialState = {
-	courses: coursesInitialState,
-};
-
-const store = createStore(rootReducer, appInitialState, composeWithDevTools());
+const store = configureStore({
+	reducer: rootReducer,
+});
 
 export default store;
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,9 +1,13 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
-import { coursesReducer } from './courses/reducer.js';
+import { userReducer } from './user/reducer';
+import { coursesReducer } from './courses/reducer';
+import { authorsReducer } from './authors/reducer';
 
 const rootReducer = combineReducers({
+	user: userReducer,
 	courses: coursesReducer,
+	authors: authorsReducer,
 });
 
 export default rootReducer;
