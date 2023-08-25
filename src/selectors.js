b5090b05c13c8f0e6f7c6b27251a2fc5ab8c09fe
Jurica Zecevic <Jurica_Zecevic@epam.com>
2023-08-25 12:17:00 +0200

    feat: Creating authors types, actions, reducer

--- src/selectors.js
+++ src/selectors.js
@@ -1,1 +1,2 @@
 export const getCourses = (state) => state.courses;
+export const getAuthors = (state) => state.authors;
--- src/services.js
+++ src/services.js
@@ -1,14 +1,19 @@
-import axios from 'axios';
-
 import { BASE_URL } from './constants';
 
-import { fetchCoursesSucceeded } from './store/courses/actions';
+import { fetchCoursesAction } from './store/courses/actions';
+import { fetchAuthorsAction } from './store/authors/actions';
 
 export const fetchCourses = () => {
 	return async (dispatch) => {
 		try {
-			const response = await axios.get(`${BASE_URL}/courses/all`);
-			dispatch(fetchCoursesSucceeded(response.data.result));
+			const response = await fetch(`${BASE_URL}/courses/all`);
+			const courses = await response.json();
+
+			if (response.ok) {
+				dispatch(fetchCoursesAction(courses.result));
+			} else {
+				throw new Error(courses.message || 'Something went wrong');
+			}
 		} catch (err) {
 			console.error('Error fetching courses:', err);
 			throw err;
@@ -16,18 +21,20 @@ export const fetchCourses = () => {
 	};
 };
 
-export const fetchAuthors = async () => {
-	try {
-		const response = await fetch(`${BASE_URL}/authors/all`);
-		const authors = await response.json();
-		if (!response.ok) {
-			throw new Error(authors.message || 'Something went wrong');
-		} else {
-			console.log(authors.result);
-			return response.data;
+export const fetchAuthors = () => {
+	return async (dispatch) => {
+		try {
+			const response = await fetch(`${BASE_URL}/authors/all`);
+			const authors = await response.json();
+
+			if (response.ok) {
+				dispatch(fetchAuthorsAction(authors.result));
+			} else {
+				throw new Error(authors.message || 'Something went wrong');
+			}
+		} catch (err) {
+			console.error('Error fetching courses:', err);
+			throw err;
 		}
-	} catch (error) {
-		console.error('Error fetching authors:', error);
-		throw error;
-	}
+	};
 };
